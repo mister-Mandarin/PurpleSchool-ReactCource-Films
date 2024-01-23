a6886486ef4856b66f8a2aa18d50bd6cdb8d7c00
Andrey <andr.poluehktov@yandex.ru>
2024-01-23 14:11:44 +0300

    оставшиеся правки

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,17 @@ export default function App() {
 	return (
 		<>
 			<Header />
-			<Input placeholder='Введите название' icon={true}/>
+			<div className="body-container">
+				<Input placeholder='Введите название' icon={true}/>
 
-			<Button name={'Искать'} onClick={() => console.log('Меня нажали!')}/>
-			<TextTitle Tag={'h1'} className={'big'} text={'Вход'} />
-			<TextParagraph className={'description-title'} text={'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'} />
+				<Button name={'Искать'} onClick={() => console.log('Меня нажали!')}/>
+				<TextTitle Tag={'h1'} className={'big'} text={'Вход'} />
+				<TextParagraph className={'description-title'} text={'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'} />
 
-			<TextTitle Tag={'h3'} className={'small'} text={'Avengers: Endgame'} />
-			<TextParagraph className={'description-card'} text={'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts...'} />
-			<CardsField />
+				<TextTitle Tag={'h3'} className={'small'} text={'Avengers: Endgame'} />
+				<TextParagraph className={'description-card'} text={'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts...'} />
+				<CardsField />
+			</div>
 		</>
 	);
 }
- 
