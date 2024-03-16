57e535a8f3d39136b503e701df8fc2c8e45cd21d
mister-Mandarin <andr.poluehktov@yandex.ru>
2024-03-16 20:42:39 +0300

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App/>,
+		children: [
+			{
+				path: '/',
+				element: <App />
+			}
+			// {
+			// 	path: '/cart',
+			// 	element: <Cart/>
+			// },
+			// {
+			// 	// после двоеточия может быть любое название
+			// 	// это значение можно достать с помощью useParams
+			// 	path: '/product/:id',
+			// 	element: <Product/>
+			// }
+		]
+	}
+	// {
+	// 	path: '*',
+	// 	element: <Error/>
+	// }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router}/>
 	</React.StrictMode>
 );
- 
